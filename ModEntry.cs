a8ad2cf090dd078f809560c3c55c384bb02139fb
Hime0321 <andrew367699@gmail.com>
2017-10-16 16:09:51 -0700

    Changes

--- ModEntry.cs
+++ ModEntry.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
 
 namespace ImmersiveCrafting
 {
